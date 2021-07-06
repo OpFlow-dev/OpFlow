17577aa15fb002d6a191ed098f2b412d20b97b6e
Luohao Wang <luohaothu@live.com>
2021-07-06 11:35:54 +0800

    Build fix

--- src/Core/Field/MeshBased/Structured/CartesianField.hpp
+++ src/Core/Field/MeshBased/Structured/CartesianField.hpp
@@ -3,8 +3,8 @@
 // Copyright (c) 2019 - 2021 by the OpFlow developers
 //
 // This file is part of OpFlow.
-// 
-// OpFlow is free software and is distributed under the MPL v2.0 license. 
+//
+// OpFlow is free software and is distributed under the MPL v2.0 license.
 // The full text of the license can be found in the file LICENSE at the top
 // level directory of OpFlow.
 //
