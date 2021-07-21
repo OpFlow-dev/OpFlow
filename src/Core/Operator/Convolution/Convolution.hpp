b53e6d0878cb273edc5cf1aff002875291382090
Luohao Wang <luohaothu@live.com>
2021-07-21 11:14:59 +0800

    Add FixedSizeTensor for later use

--- src/Core/Operator/Convolution/Convolution.hpp
+++ src/Core/Operator/Convolution/Convolution.hpp
@@ -15,9 +15,15 @@
 
 #include "Core/Field/MeshBased/SemiStructured/CartAMRFieldExprTrait.hpp"
 #include "Core/Field/MeshBased/Structured/CartesianFieldExprTrait.hpp"
+#include "Core/Field/MeshBased/Structured/StructuredFieldExprTrait.hpp"
+#include "Core/Meta.hpp"
+#include "DataStructures/Arrays/Tensor/FixedSizeTensor.hpp"
 
 namespace OpFlow {
     template <std::size_t d>
-    struct Convolution {};
+    struct Convolution {
+        template <StructuredFieldExprType E, Meta::Numerical K, typename I>
+        OPFLOW_STRONG_INLINE static auto eval(const E& e, const DS::FixedSizeTensor<K, d>& kernel, I&& i) {}
+    };
 }// namespace OpFlow
 #endif//OPFLOW_CONVOLUTION_HPP
