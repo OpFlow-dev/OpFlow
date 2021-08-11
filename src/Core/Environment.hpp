b2f682eb4c3cd63f234668562a019fc2c61a6740
Luohao Wang <luohaothu@live.com>
2021-08-11 22:27:52 +0800

    Add Environment.hpp for global states

--- src/Core/Environment.hpp
+++ src/Core/Environment.hpp
@@ -17,6 +17,9 @@
 #include <mpi.h>
 #endif
 
+#include "Core/Parallel/ParallelInfo.hpp"
+#include "Core/Parallel/ParallelPlan.hpp"
+
 namespace OpFlow {
     void static inline InitEnvironment(int argc, char** argv) {
 #ifdef OPFLOW_WITH_MPI
@@ -28,6 +31,27 @@ namespace OpFlow {
 #ifdef OPFLOW_WITH_MPI
         MPI_Finalize();
 #endif
+    }
+
+    namespace internal {
+        static inline ParallelInfo GLOBAL_PARALLELINFO;
+        static inline ParallelPlan GLOBAL_PARALLELPLAN;
+    }
+
+    inline static auto& getGlobalParallelInfo() {
+        return internal::GLOBAL_PARALLELINFO;
+    }
+
+    inline static auto& getGlobalParallelPlan() {
+        return internal::GLOBAL_PARALLELPLAN;
+    }
+
+    inline static void setGlobalParallelInfo(const ParallelInfo& info) {
+        internal::GLOBAL_PARALLELINFO = info;
+    }
+
+    inline static void setGlobalParallelPlan(const ParallelPlan& plan) {
+        internal::GLOBAL_PARALLELPLAN = plan;
     }
 }
 #endif//OPFLOW_ENVIRONMENT_HPP
