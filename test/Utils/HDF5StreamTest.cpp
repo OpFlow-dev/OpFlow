5964dc5f6b4f8bb4727c70a252d9774aee7122a1
Luohao Wang <Luohaothu@users.noreply.github.com>
2021-09-24 15:34:42 +0800

    Fix HDF5 transpose bug (#8)

--- test/Utils/HDF5StreamTest.cpp
+++ test/Utils/HDF5StreamTest.cpp
@@ -13,7 +13,7 @@
 #include <OpFlow>
 #include <gmock/gmock.h>
 
-TEST(H5RWTest, WriteAfterRead) {
+TEST(H5RWTest, ReadAfterWrite) {
     using namespace OpFlow;
     using Mesh = CartesianMesh<Meta::int_<2>>;
     using Field = CartesianField<Real, Mesh>;
@@ -38,6 +38,37 @@ TEST(H5RWTest, WriteAfterRead) {
     auto v = u;
     v = 0.;
     Utils::H5Stream istream("./u.h5", StreamIn);
-    stream >> v;
+    istream >> v;
+    ASSERT_EQ(v.evalAt(DS::MDIndex<2>(2, 2)), u.evalAt(DS::MDIndex<2>(2, 2)));
+}
+
+TEST(H5RWTest, ReadAfterWriteInEqualDim) {
+    using namespace OpFlow;
+    using Mesh = CartesianMesh<Meta::int_<2>>;
+    using Field = CartesianField<Real, Mesh>;
+
+    auto mesh = MeshBuilder<Mesh>().newMesh(5, 7).setMeshOfDim(0, 0., 1.).setMeshOfDim(1, 0., 1.).build();
+    auto u = ExprBuilder<Field>()
+                     .setName("u")
+                     .setMesh(mesh)
+                     .setBC(0, DimPos::start, BCType::Dirc, 1.)
+                     .setBC(0, DimPos::end, BCType::Dirc, 1.)
+                     .setBC(1, DimPos::start, BCType::Dirc, 1.)
+                     .setBC(1, DimPos::end, BCType::Dirc, 1.)
+                     .setLoc(std::array {LocOnMesh ::Center, LocOnMesh ::Center})
+                     .build();
+
+    u = 0.;
+    auto map = DS::MDRangeMapper<2> {u.accessibleRange};
+    rangeFor(u.assignableRange, [&](auto&& i) { u[i] = map(i); });
+    Utils::H5Stream stream("./u_ieq.h5");
+    stream << u;
+    stream.close();
+
+    auto v = u;
+    v = 0.;
+    Utils::H5Stream istream("./u_ieq.h5", StreamIn);
+    istream >> v;
+    istream.close();
     ASSERT_EQ(v.evalAt(DS::MDIndex<2>(2, 2)), u.evalAt(DS::MDIndex<2>(2, 2)));
 }
