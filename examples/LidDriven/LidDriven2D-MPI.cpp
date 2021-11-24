89b099f2c171552c3482094a0e50ad020263139b
Luohao Wang <luohaothu@live.com>
2021-11-24 22:23:50 +0800

    [Example] Fix the coefficient error

--- examples/LidDriven/LidDriven2D-MPI.cpp
+++ examples/LidDriven/LidDriven2D-MPI.cpp
@@ -124,7 +124,7 @@ int main(int argc, char* argv[]) {
     for (auto i = 0; i < 100; ++i) {
         u_handler.solve();
         v_handler.solve();
-        du = du - dt * conv_xy(u, dv);
+        du = du - 0.5 * dt * conv_xy(u, dv);
         u = u + du;
         v = v + dv;
         p_handler.solve();
