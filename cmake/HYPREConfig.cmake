67688135b21d13b9c8ec82c3de5e4b20762d5e77
Luohao Wang <luohaothu@live.com>
2021-12-03 14:10:30 +0800

    [Build] Refactor external libraries' build logic

--- cmake/HYPREConfig.cmake
+++ cmake/HYPREConfig.cmake
@@ -14,46 +14,45 @@
 #
 # ----------------------------------------------------------------------------
 
-macro(CONFIG_HYPRE)
-    # try build HYPRE from source
+macro(CONFIG_AND_INSTALL_HYPRE)
+    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/hypre-build)
     string(JOIN " " HYPRE_OPTIONS
             "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}"
             "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}"
             "-DHYPRE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
             "-DHYPRE_ENABLE_SINGLE=${OPFLOW_SINGLE_PRECISION}"
             "-DHYPRE_WITH_OPENMP=${OPFLOW_WITH_OPENMP}"
-            "-DHYPRE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/hypre")
+            "-DHYPRE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}")
     string(REPLACE " " "\t\n" _hypre_options ${HYPRE_OPTIONS})
     message(STATUS "HYPRE is to be configured with:\t\n${_hypre_options}")
 
-    set(OPFLOW_HYPRE_PRE_DOWNLOAD ON)
     set(OPFLOW_HYPRE_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/external/hypre)
 
-    if (OPFLOW_USE_BUNDLED_HYPRE)
-        # use pre-downloaded source file
-        configure_file(cmake/UseBundledHYPRE.cmake.in
-                ${CMAKE_CURRENT_BINARY_DIR}/hypre-download/CMakeLists.txt)
-    else ()
-        configure_file(cmake/DownloadHYPRE.cmake.in
-                ${CMAKE_CURRENT_BINARY_DIR}/hypre-download/CMakeLists.txt)
-    endif ()
-    execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
+    execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}"
+            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            -DHYPRE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DHYPRE_ENABLE_SINGLE=${OPFLOW_SINGLE_PRECISION}
+            -DHYPRE_WITH_OPENMP=${OPFLOW_WITH_OPENMP}
+            -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+            ${CMAKE_CURRENT_SOURCE_DIR}/external/hypre/src
             RESULT_VARIABLE _result
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/hypre-download)
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/hypre-build)
     if (_result)
         message(FATAL_ERROR "CMake step for HYPRE failed: ${_result}")
     endif ()
 
     execute_process(COMMAND ${CMAKE_COMMAND} --build . -j
             RESULT_VARIABLE _result
-            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/hypre-download)
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/hypre-build)
     if (_result)
         message(FATAL_ERROR "Build step for HYPRE failed: ${_result}")
     endif ()
 
-    if (OPFLOW_WITH_OPENMP)
-        set(CMAKE_REQUIRED_FLAGS "-fopenmp")
+    execute_process(COMMAND ${CMAKE_COMMAND} --install .
+            RESULT_VARIABLE _result
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/hypre-build)
+    if (_result)
+        message(FATAL_ERROR "Install step for HYPRE failed: ${_result}")
     endif ()
-    find_package(HYPRE CONFIG REQUIRED
-            PATHS ${CMAKE_CURRENT_BINARY_DIR}/hypre/lib/cmake/HYPRE)
+
 endmacro()
