1eb83eec54014c34fa5f7a973b9ae1a4742475f7
Luohao Wang <luohaothu@live.com>
2021-12-03 23:02:39 +0800

    [Bench] Add Laplace benchmark

--- benchmark/Core/LaplaceOp.cpp
+++ benchmark/Core/LaplaceOp.cpp
@@ -9,11 +9,25 @@
 //  level directory of OpFlow.
 //
 //  ----------------------------------------------------------------------------
+//
+//
+
+//  ----------------------------------------------------------------------------
+//
+//  Copyright (c) 2019 - 2021  by the OpFlow developers
+//
+//  This file is part of OpFlow.
+//
+//  OpFlow is free software and is distributed under the MPL v2.0 license.
+//  The full text of the license can be found in the file LICENSE at the top
+//  level directory of OpFlow.
+//
+//  ----------------------------------------------------------------------------
 
 #include <OpFlow>
 #include <benchmark/benchmark.h>
 
-static void UniFieldAssign_2d(benchmark::State& state) {
+static void Laplace_2d(benchmark::State& state) {
     using namespace OpFlow;
 
     using Mesh = CartesianMesh<Meta::int_<2>>;
@@ -26,21 +40,27 @@ static void UniFieldAssign_2d(benchmark::State& state) {
     auto u = ExprBuilder<Field>()
                      .setMesh(m)
                      .setLoc(std::array {LocOnMesh::Center, LocOnMesh::Center})
+                     .setBC(0, DimPos::start, BCType::Periodic)
+                     .setBC(0, DimPos::end, BCType::Periodic)
+                     .setBC(1, DimPos::start, BCType::Periodic)
+                     .setBC(1, DimPos::end, BCType::Periodic)
+                     .setExt(1)
                      .build();
+    u = 1.0;
     auto v = u;
 
     omp_set_num_threads(state.range(1));
-    for (auto _ : state) { u = v; }
+    for (auto _ : state) { u = d2x<D2SecondOrderCentered>(v) + d2y<D2SecondOrderCentered>(v); }
 }
 
-static void UniFieldAssign_2d_Params(benchmark::internal::Benchmark* b) {
+static void Laplace_2d_Params(benchmark::internal::Benchmark* b) {
     for (auto i = 8; i <= 8 << 10; i *= 2)
         for (auto j = 1; j <= omp_get_max_threads(); j *= 2) b->Args({i + 1, j});
 }
 
-BENCHMARK(UniFieldAssign_2d)->Apply(UniFieldAssign_2d_Params)->UseRealTime();
+BENCHMARK(Laplace_2d)->Apply(Laplace_2d_Params)->UseRealTime();
 
-static void UniFieldAssign_3d(benchmark::State& state) {
+static void Laplace_3d(benchmark::State& state) {
     using namespace OpFlow;
 
     using Mesh = CartesianMesh<Meta::int_<3>>;
@@ -58,18 +78,28 @@ static void UniFieldAssign_3d(benchmark::State& state) {
     auto u = ExprBuilder<Field>()
                      .setMesh(m)
                      .setLoc(std::array {LocOnMesh::Center, LocOnMesh::Center, LocOnMesh::Center})
+                     .setBC(0, DimPos::start, BCType::Periodic)
+                     .setBC(0, DimPos::end, BCType::Periodic)
+                     .setBC(1, DimPos::start, BCType::Periodic)
+                     .setBC(1, DimPos::end, BCType::Periodic)
+                     .setBC(2, DimPos::start, BCType::Periodic)
+                     .setBC(2, DimPos::end, BCType::Periodic)
+                     .setExt(1)
                      .build();
+    u = 1.0;
     auto v = u;
 
     omp_set_num_threads(state.range(1));
-    for (auto _ : state) { u = v; }
+    for (auto _ : state) {
+        u = d2x<D2SecondOrderCentered>(v) + d2y<D2SecondOrderCentered>(v) + d2z<D2SecondOrderCentered>(v);
+    }
 }
 
-static void UniFieldAssign_3d_Params(benchmark::internal::Benchmark* b) {
+static void Laplace_3d_Params(benchmark::internal::Benchmark* b) {
     for (auto i = 8; i <= 512; i *= 2)
         for (auto j = 1; j <= omp_get_max_threads(); j *= 2) b->Args({i + 1, j});
 }
 
-//BENCHMARK(UniFieldAssign_3d)->Apply(UniFieldAssign_3d_Params)->UseRealTime();
+//BENCHMARK(Laplace_3d)->Apply(Laplace_3d_Params)->UseRealTime();
 
 BENCHMARK_MAIN();
