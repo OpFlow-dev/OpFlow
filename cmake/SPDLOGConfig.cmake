166104723677951d51be584184c9ed0c7296070f
Luohao Wang <luohaothu@live.com>
2021-12-03 14:10:30 +0800

    [Build] Refactor external libraries' build logic

---
+++ cmake/SPDLOGConfig.cmake
@@ -0,0 +1,42 @@
+# ----------------------------------------------------------------------------
+#
+# Copyright (c) 2019 - 2021 by the OpFlow developers
+#
+# This file is part of OpFlow.
+#
+# OpFlow is free software and is distributed under the MPL v2.0 license.
+# The full text of the license can be found in the file LICENSE at the top
+# level directory of OpFlow.
+#
+# ----------------------------------------------------------------------------
+#
+# Config & build spdlog from source
+#
+# ----------------------------------------------------------------------------
+
+macro(CONFIG_AND_INSTALL_SPDLOG)
+    file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/spdlog-build)
+    execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}"
+            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
+            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+            -DSPDLOG_BUILD_EXAMPLE=OFF
+            -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${CMAKE_CURRENT_SOURCE_DIR}/external/spdlog
+            RESULT_VARIABLE result
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/spdlog-build)
+    if (result)
+        message(FATAL_ERROR "CMake step for spdlog failed: ${result}")
+    endif ()
+    execute_process(COMMAND ${CMAKE_COMMAND} --build . -j
+            RESULT_VARIABLE result
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/spdlog-build)
+    if (result)
+        message(FATAL_ERROR "Build step for spdlog failed: ${result}")
+    endif ()
+    execute_process(COMMAND ${CMAKE_COMMAND} --install .
+            RESULT_VARIABLE result
+            WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/spdlog-build)
+    if (result)
+        message(FATAL_ERROR "Install step for spdlog failed: ${result}")
+    endif ()
+endmacro()
