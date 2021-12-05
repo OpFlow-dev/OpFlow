254f3c8c20b7f9913c5c46065c0bca173a88d413
Luohao Wang <luohaothu@users.noreply.github.com>
2021-12-05 17:00:59 +0800

    [Test] Add test for 1D&3D EvenSplitStrategyTest

--- test/Core/Parallel/EvenSplitStrategyTest.cpp
+++ test/Core/Parallel/EvenSplitStrategyTest.cpp
@@ -84,3 +84,27 @@ TEST_F(EvenSplitStrategyTest, UnDivisibleDimInEqual) {
     ASSERT_TRUE((map[4] == DS::Range<2> {std::array {16, 16}, std::array {32, 33}}));
     ASSERT_TRUE((map[5] == DS::Range<2> {std::array {32, 16}, std::array {49, 33}}));
 }
+
+TEST_F(EvenSplitStrategyTest, 1DTest) {
+    auto range = DS::Range<1>{std::array<int, 1>{17}};
+    auto plan = ParallelPlan{};
+    plan.distributed_workers_count = 4;
+    setGlobalParallelPlan(plan);
+    auto strategy = EvenSplitStrategy<CartesianField<double, CartesianMesh<Meta::int_<1>>>>{};
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_TRUE((map[0] == DS::Range<1>{std::array{4}}));
+    ASSERT_TRUE((map[1] == DS::Range<1>{std::array{4}, std::array{8}}));
+    ASSERT_TRUE((map[2] == DS::Range<1>{std::array{8}, std::array{12}}));
+    ASSERT_TRUE((map[3] == DS::Range<1>{std::array{12}, std::array{16}}));
+}
+
+TEST_F(EvenSplitStrategyTest, 3DTest) {
+    auto range = DS::Range<3>{std::array<int, 3>{5, 6, 7}};
+    auto plan = ParallelPlan{};
+    plan.distributed_workers_count = 6;
+    setGlobalParallelPlan(plan);
+    auto strategy = EvenSplitStrategy<CartesianField<double, CartesianMesh<Meta::int_<3>>>>{};
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_EQ(map[0], (DS::Range<3>{std::array{4, 2, 2}}));
+    ASSERT_EQ(map[5], (DS::Range<3>{std::array{0, 2, 4}, std::array{4, 5, 6}}));
+}
