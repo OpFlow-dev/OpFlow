1ef7902f3ace22eff4fc836be5c7b9a4f6286291
Luohao Wang <Luohaothu@users.noreply.github.com>
2022-02-28 23:11:04 +0800

    [CI] Add AMGCLMPITest

---
+++ test/Core/Equation/AMGCLMPITest.cpp
@@ -0,0 +1,20 @@
+//  ----------------------------------------------------------------------------
+//
+//  Copyright (c) 2019 - 2022 by the OpFlow developers
+//
+//  This file is part of OpFlow.
+//
+//  OpFlow is free software and is distributed under the MPL v2.0 license.
+//  The full text of the license can be found in the file LICENSE at the top
+//  level directory of OpFlow.
+//
+//  ----------------------------------------------------------------------------
+//
+//
+
+#include <OpFlow>
+#include <gmock/gmock.h>
+
+using namespace OpFlow;
+
+TEST(AMGCLMPITest, DirectSolve) { ASSERT_TRUE(true); }
