eb38ac9eb534bc9ac57971bd9170e78028be331c
Luohao Wang <Luohaothu@users.noreply.github.com>
2022-07-08 11:01:55 +0800

    [Bench] Add more benchmarks for matgen

--- benchmark/Core/EqnSolveMPI.cpp
+++ benchmark/Core/EqnSolveMPI.cpp
@@ -24,7 +24,7 @@ public:
         auto n = state.range(0);
         auto m = MeshBuilder<Mesh>().newMesh(n, n).setMeshOfDim(0, 0., 1.).setMeshOfDim(1, 0., 1.).build();
         std::shared_ptr<AbstractSplitStrategy<Field>> s = std::make_shared<EvenSplitStrategy<Field>>();
-        u = std::make_shared<Field>(ExprBuilder<Field>()
+        p = std::make_shared<Field>(ExprBuilder<Field>()
                                             .setMesh(m)
                                             .setLoc(std::array {LocOnMesh::Center, LocOnMesh::Center})
                                             .setBC(0, DimPos::start, BCType::Dirc, 0.)
@@ -35,13 +35,68 @@ public:
                                             .setPadding(1)
                                             .setSplitStrategy(s)
                                             .build());
+        u = std::make_shared<Field>(ExprBuilder<Field>()
+                                            .setMesh(m)
+                                            .setLoc(std::array {LocOnMesh::Corner, LocOnMesh::Center})
+                                            .setBC(0, DimPos::start, BCType::Dirc, 0.)
+                                            .setBC(0, DimPos::end, BCType::Dirc, 0.)
+                                            .setBC(1, DimPos::start, BCType::Dirc, 0.)
+                                            .setBC(1, DimPos::end, BCType::Dirc, 0.)
+                                            .setExt(1)
+                                            .setPadding(1)
+                                            .setSplitStrategy(s)
+                                            .build());
+        v = std::make_shared<Field>(ExprBuilder<Field>()
+                                            .setMesh(m)
+                                            .setLoc(std::array {LocOnMesh::Center, LocOnMesh::Corner})
+                                            .setBC(0, DimPos::start, BCType::Dirc, 0.)
+                                            .setBC(0, DimPos::end, BCType::Dirc, 0.)
+                                            .setBC(1, DimPos::start, BCType::Dirc, 0.)
+                                            .setBC(1, DimPos::end, BCType::Dirc, 0.)
+                                            .setExt(1)
+                                            .setPadding(1)
+                                            .setSplitStrategy(s)
+                                            .build());
+        r = std::make_shared<Field>(ExprBuilder<Field>()
+                                            .setMesh(m)
+                                            .setLoc(std::array {LocOnMesh::Center, LocOnMesh::Center})
+                                            .setBC(0, DimPos::start, BCType::Dirc, 1.)
+                                            .setBC(0, DimPos::end, BCType::Dirc, 1.)
+                                            .setBC(1, DimPos::start, BCType::Dirc, 1.)
+                                            .setBC(1, DimPos::end, BCType::Dirc, 1.)
+                                            .setExt(1)
+                                            .setPadding(2)
+                                            .setSplitStrategy(s)
+                                            .build());
+        rx = std::make_shared<Field>(ExprBuilder<Field>()
+                                             .setMesh(m)
+                                             .setLoc(std::array {LocOnMesh::Corner, LocOnMesh::Center})
+                                             .setBC(0, DimPos::start, BCType::Dirc, 1.)
+                                             .setBC(0, DimPos::end, BCType::Dirc, 1.)
+                                             .setBC(1, DimPos::start, BCType::Dirc, 1.)
+                                             .setBC(1, DimPos::end, BCType::Dirc, 1.)
+                                             .setExt(1)
+                                             .setPadding(2)
+                                             .setSplitStrategy(s)
+                                             .build());
+        ry = std::make_shared<Field>(ExprBuilder<Field>()
+                                             .setMesh(m)
+                                             .setLoc(std::array {LocOnMesh::Center, LocOnMesh::Corner})
+                                             .setBC(0, DimPos::start, BCType::Dirc, 1.)
+                                             .setBC(0, DimPos::end, BCType::Dirc, 1.)
+                                             .setBC(1, DimPos::start, BCType::Dirc, 1.)
+                                             .setBC(1, DimPos::end, BCType::Dirc, 1.)
+                                             .setExt(1)
+                                             .setPadding(2)
+                                             .setSplitStrategy(s)
+                                             .build());
         params.p.solver.tol = 1e-16;
         params.p.solver.maxiter = 100;
         handler = makeEqnSolveHandler<Solver>(
                 [&](auto&& e) {
                     return d2x<D2SecondOrderCentered>(e) + d2y<D2SecondOrderCentered>(e) == 1.0;
                 },
-                *u, DS::BlockedMDRangeMapper<2> {u->getLocalWritableRange()}, params);
+                *p, DS::BlockedMDRangeMapper<2> {p->getLocalWritableRange()}, params);
     }
 
     auto gatherTime(const auto& start, const auto& end) {
@@ -57,7 +112,7 @@ public:
         handler = nullptr;
     }
 
-    std::shared_ptr<Field> u;
+    std::shared_ptr<Field> p, u, v, r, rx, ry;
     typedef amgcl::backend::builtin<double> SBackend;
 #ifdef MIXED_PRECISION
     typedef amgcl::backend::builtin<float> PBackend;
@@ -82,17 +137,86 @@ BENCHMARK_DEFINE_F(AMGCLEqnSolveBench, matgen)(benchmark::State& state) {
     }
 }
 
+BENCHMARK_DEFINE_F(AMGCLEqnSolveBench, matgen_sppoisson)(benchmark::State& state) {
+    double dt = 0.1;
+    u->initBy([&](auto&& x) { return std::sin(x[0] * 2 * PI) * std::sin(x[1] * 2 * PI); });
+    v->initBy([&](auto&& x) { return std::cos(x[0] * 2 * PI) * std::cos(x[1] * 2 * PI); });
+    *p = 0;
+    handler = makeEqnSolveHandler<Solver>(
+            [&](auto&& e) {
+                return d2x<D2SecondOrderCentered>(e) + d2y<D2SecondOrderCentered>(e)
+                       == (dx<D1FirstOrderCentered>(*u) + dy<D1FirstOrderCentered>(*v)) / dt;
+            },
+            *p, DS::BlockedMDRangeMapper<2> {p->getLocalWritableRange()}, params);
+
+    while (state.KeepRunning()) {
+        auto start = std::chrono::high_resolution_clock::now();
+        handler->generateAb();
+        auto end = std::chrono::high_resolution_clock::now();
+        state.SetIterationTime(gatherTime(start, end));
+    }
+}
+
+BENCHMARK_DEFINE_F(AMGCLEqnSolveBench, matgen_tppoisson)(benchmark::State& state) {
+    double dt = 0.1;
+    u->initBy([&](auto&& x) { return std::sin(x[0] * 2 * PI) * std::sin(x[1] * 2 * PI); });
+    v->initBy([&](auto&& x) { return std::cos(x[0] * 2 * PI) * std::cos(x[1] * 2 * PI); });
+    *p = 0;
+    *r = 1;
+    *rx = 1;
+    *ry = 1;
+    handler = makeEqnSolveHandler<Solver>(
+            [&](auto&& e) {
+                return dx<D1FirstOrderCentered>(dx<D1FirstOrderCentered>(e) / d1IntpCenterToCorner<0>(*r))
+                               + dy<D1FirstOrderCentered>(dy<D1FirstOrderCentered>(e)
+                                                          / d1IntpCenterToCorner<1>(*r))
+                       == (dx<D1FirstOrderCentered>(*u) + dy<D1FirstOrderCentered>(*v)) / dt;
+            },
+            *p, DS::BlockedMDRangeMapper<2> {p->getLocalWritableRange()}, params);
+
+    while (state.KeepRunning()) {
+        auto start = std::chrono::high_resolution_clock::now();
+        handler->generateAb();
+        auto end = std::chrono::high_resolution_clock::now();
+        state.SetIterationTime(gatherTime(start, end));
+    }
+}
+
+BENCHMARK_DEFINE_F(AMGCLEqnSolveBench, matgen_tpcpoisson)(benchmark::State& state) {
+    double dt = 0.1;
+    u->initBy([&](auto&& x) { return std::sin(x[0] * 2 * PI) * std::sin(x[1] * 2 * PI); });
+    v->initBy([&](auto&& x) { return std::cos(x[0] * 2 * PI) * std::cos(x[1] * 2 * PI); });
+    *p = 0;
+    *r = 1;
+    *rx = 1;
+    *ry = 1;
+    handler = makeEqnSolveHandler<Solver>(
+            [&](auto&& e) {
+                return dx<D1FirstOrderCentered>(dx<D1FirstOrderCentered>(e) / *rx)
+                               + dy<D1FirstOrderCentered>(dy<D1FirstOrderCentered>(e) / *ry)
+                       == (dx<D1FirstOrderCentered>(*u) + dy<D1FirstOrderCentered>(*v)) / dt;
+            },
+            *p, DS::BlockedMDRangeMapper<2> {p->getLocalWritableRange()}, params);
+
+    while (state.KeepRunning()) {
+        auto start = std::chrono::high_resolution_clock::now();
+        handler->generateAb();
+        auto end = std::chrono::high_resolution_clock::now();
+        state.SetIterationTime(gatherTime(start, end));
+    }
+}
+
 BENCHMARK_DEFINE_F(AMGCLEqnSolveBench, solve)(benchmark::State& state) {
     params.p.solver.maxiter = 10;
     params.p.solver.tol = 1e-50;
     params.staticMat = false;
     handler = makeEqnSolveHandler<Solver>(
             [&](auto&& e) { return d2x<D2SecondOrderCentered>(e) + d2y<D2SecondOrderCentered>(e) == 1.0; },
-            *u, DS::BlockedMDRangeMapper<2> {u->getLocalWritableRange()}, params);
+            *p, DS::BlockedMDRangeMapper<2> {p->getLocalWritableRange()}, params);
     auto [iter, err, abserr] = handler->solve();
 
     while (state.KeepRunning()) {
-        *u = 0.;
+        *p = 0.;
         auto start = std::chrono::high_resolution_clock::now();
         handler->solve();
         auto end = std::chrono::high_resolution_clock::now();
@@ -105,14 +229,14 @@ BENCHMARK_DEFINE_F(AMGCLEqnSolveBench, dy_solve)(benchmark::State& state) {
     params.p.solver.maxiter = 10;
     params.p.solver.tol = 1e-50;
     params.staticMat = true;
-    *u = 0;
+    *p = 0;
     handler = makeEqnSolveHandler<Solver>(
             [&](auto&& e) { return d2x<D2SecondOrderCentered>(e) + d2y<D2SecondOrderCentered>(e) == 1.0; },
-            *u, DS::BlockedMDRangeMapper<2> {u->getLocalWritableRange()}, params);
+            *p, DS::BlockedMDRangeMapper<2> {p->getLocalWritableRange()}, params);
     auto [iter, err, abserr] = handler->solve();
 
     while (state.KeepRunning()) {
-        *u = 0.;
+        *p = 0.;
         auto start = std::chrono::high_resolution_clock::now();
         handler->solve();
         auto end = std::chrono::high_resolution_clock::now();
@@ -125,6 +249,18 @@ static void EqnSolve_2d_Params(benchmark::internal::Benchmark* b) {
 }
 
 BENCHMARK_REGISTER_F(AMGCLEqnSolveBench, matgen)
+        ->Apply(EqnSolve_2d_Params)
+        ->UseManualTime()
+        ->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(AMGCLEqnSolveBench, matgen_sppoisson)
+        ->Apply(EqnSolve_2d_Params)
+        ->UseManualTime()
+        ->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(AMGCLEqnSolveBench, matgen_tppoisson)
+        ->Apply(EqnSolve_2d_Params)
+        ->UseManualTime()
+        ->Unit(benchmark::kMillisecond);
+BENCHMARK_REGISTER_F(AMGCLEqnSolveBench, matgen_tpcpoisson)
         ->Apply(EqnSolve_2d_Params)
         ->UseManualTime()
         ->Unit(benchmark::kMillisecond);
