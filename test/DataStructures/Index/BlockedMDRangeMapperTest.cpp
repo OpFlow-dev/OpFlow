e337ba8959029475593e20ec4fba0f43452a7509
Luohao Wang <Luohaothu@users.noreply.github.com>
2022-10-29 00:17:54 +0800

    [DS] Add test for BlockedMDRangeMapperTest with different splits & var locations

--- test/DataStructures/Index/BlockedMDRangeMapperTest.cpp
+++ test/DataStructures/Index/BlockedMDRangeMapperTest.cpp
@@ -52,7 +52,7 @@ protected:
     Mesh m;
 };
 
-TEST_F(BlockedMDRangeMapperTest, XFaceMap) {
+TEST_F(BlockedMDRangeMapperTest, XFaceMap_N3) {
     std::vector<DS::Range<2>> ranges;
     auto info = makeParallelInfo();
     info.nodeInfo.node_count = 3;
@@ -65,4 +65,67 @@ TEST_F(BlockedMDRangeMapperTest, XFaceMap) {
     // Block 3
     ASSERT_EQ(mapper(DS::MDIndex<2> {2, 0}), 4);
     ASSERT_EQ(mapper(DS::MDIndex<2> {3, 0}), 5);
+}
+
+TEST_F(BlockedMDRangeMapperTest, XFaceMap_N4) {
+    std::vector<DS::Range<2>> ranges;
+    auto info = makeParallelInfo();
+    info.nodeInfo.node_count = 4;
+    auto plan = makeParallelPlan(info, ParallelIdentifier::DistributeMem);
+    auto splitMap = strategy->getSplitMap(m.getRange(), plan);
+    for (const auto& m : splitMap) { ranges.push_back(DS::commonRange(m, u.getLocalWritableRange())); }
+    auto mapper = DS::BlockedMDRangeMapper<2>(ranges);
+    // Block 1
+    ASSERT_EQ(mapper(DS::MDIndex<2> {1, 0}), 0);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {1, 1}), 1);
+    // Block 2
+    ASSERT_EQ(mapper(DS::MDIndex<2> {2, 0}), 2);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {3, 0}), 3);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {3, 1}), 5);
+    // Block 3
+    ASSERT_EQ(mapper(DS::MDIndex<2> {1, 2}), 6);
+    // Block 4
+    ASSERT_EQ(mapper(DS::MDIndex<2> {2, 2}), 8);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {3, 3}), 11);
+}
+
+TEST_F(BlockedMDRangeMapperTest, CenterMap_N3) {
+    std::vector<DS::Range<2>> ranges;
+    auto info = makeParallelInfo();
+    info.nodeInfo.node_count = 3;
+    auto plan = makeParallelPlan(info, ParallelIdentifier::DistributeMem);
+    auto splitMap = strategy->getSplitMap(m.getRange(), plan);
+    for (const auto& m : splitMap) { ranges.push_back(DS::commonRange(m, p.getLocalWritableRange())); }
+    auto mapper = DS::BlockedMDRangeMapper<2>(ranges);
+    // Block 1
+    ASSERT_EQ(mapper(DS::MDIndex<2> {0, 0}), 0);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {0, 3}), 3);
+    // Block 2
+    ASSERT_EQ(mapper(DS::MDIndex<2> {1, 0}), 4);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {1, 3}), 7);
+    // Block 3
+    ASSERT_EQ(mapper(DS::MDIndex<2> {2, 0}), 8);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {3, 3}), 15);
+}
+
+TEST_F(BlockedMDRangeMapperTest, CenterMap_N4) {
+    std::vector<DS::Range<2>> ranges;
+    auto info = makeParallelInfo();
+    info.nodeInfo.node_count = 4;
+    auto plan = makeParallelPlan(info, ParallelIdentifier::DistributeMem);
+    auto splitMap = strategy->getSplitMap(m.getRange(), plan);
+    for (const auto& m : splitMap) { ranges.push_back(DS::commonRange(m, p.getLocalWritableRange())); }
+    auto mapper = DS::BlockedMDRangeMapper<2>(ranges);
+    // Block 1
+    ASSERT_EQ(mapper(DS::MDIndex<2> {0, 0}), 0);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {1, 1}), 3);
+    // Block 2
+    ASSERT_EQ(mapper(DS::MDIndex<2> {2, 0}), 4);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {3, 1}), 7);
+    // Block 3
+    ASSERT_EQ(mapper(DS::MDIndex<2> {0, 2}), 8);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {1, 3}), 11);
+    // Block 4
+    ASSERT_EQ(mapper(DS::MDIndex<2> {2, 2}), 12);
+    ASSERT_EQ(mapper(DS::MDIndex<2> {3, 3}), 15);
 }
