7902b94d6070032709171762d6f5356ffa00c1d8
Luohao Wang <Luohaothu@users.noreply.github.com>
2022-12-26 20:55:56 +0800

    [Core] Add ParticleGuidedSplitStrategy & tests

---
+++ src/Core/Parallel/ParticleGuidedSplitStrategy.hpp
@@ -0,0 +1,259 @@
+//  ----------------------------------------------------------------------------
+//
+//  Copyright (c) 2019 - 2022 by the OpFlow developers
+//
+//  This file is part of OpFlow.
+//
+//  OpFlow is free software and is distributed under the MPL v2.0 license.
+//  The full text of the license can be found in the file LICENSE at the top
+//  level directory of OpFlow.
+//
+//  ----------------------------------------------------------------------------
+
+#ifndef OPFLOW_PARTICLEGUIDEDSPLITSTRATEGY_HPP
+#define OPFLOW_PARTICLEGUIDEDSPLITSTRATEGY_HPP
+
+#include "Core/Field/MeshBased/Structured/CartesianFieldTrait.hpp"
+#include "Core/Parallel/AbstractSplitStrategy.hpp"
+#ifdef OPFLOW_WITH_MPI
+#include <mpi.h>
+#endif
+#include <array>
+#include <bit>
+#include <string>
+#include <vector>
+
+namespace OpFlow {
+    template <int d>
+    struct Particle {
+        std::array<double, d> x;
+    };
+
+    template <CartesianFieldType F>
+    struct ParticleGuidedSplitStrategy : AbstractSplitStrategy<F> {
+    public:
+        static constexpr int dim = internal::ExprTrait<F>::dim;
+
+        ParticleGuidedSplitStrategy() = default;
+        ParticleGuidedSplitStrategy(std::vector<Particle<dim>> parts, double part_load, double mesh_laod,
+                                    int max_levels,
+                                    const typename internal::ExprTrait<F>::mesh_type& ref_mesh)
+            : particles(parts), part_load_factor(part_load), mesh_load_factor(mesh_laod),
+              max_levels(max_levels), ref_mesh(ref_mesh) {}
+
+        [[nodiscard]] std::string strategyName() const override { return "Particle guided split"; }
+        typename internal::ExprTrait<F>::range_type
+        splitRange(const typename internal::ExprTrait<F>::range_type& range,
+                   const ParallelPlan& plan) override {
+            check_state(plan);
+            return split_impl(range, plan);
+        }
+
+        std::vector<typename internal::ExprTrait<F>::range_type>
+        getSplitMap(const typename internal::ExprTrait<F>::range_type& range,
+                    const ParallelPlan& plan) override {
+            check_state(plan);
+            return splitMap_impl(range, plan);
+        }
+
+        void setParticles(std::vector<Particle<dim>> parts) { particles = std::move(parts); }
+
+        void setParticleLoad(double load) { part_load_factor = load; }
+
+        void setMeshLoad(double load) { mesh_load_factor = load; }
+
+        void setRefMesh(const typename internal::ExprTrait<F>::mesh_type& mesh) { ref_mesh = mesh; }
+
+        void setMaxLevel(int level) { max_levels = level; }
+
+    private:
+        std::vector<Particle<dim>> particles;
+        typename internal::ExprTrait<F>::mesh_type ref_mesh;
+        double part_load_factor = 0, mesh_load_factor = 1;
+        int max_levels = 0;
+
+        struct Node {
+            std::unique_ptr<Node> lc, rc;
+            Node* parent = nullptr;
+            DS::Range<dim> range;
+            std::vector<Particle<dim>> particles;
+            int split_axis = -1;// -1 for leaf node
+
+            Node() = default;
+            void traverse(auto&& func) const {
+                if (!is_leaf()) {
+                    lc->traverse(OP_PERFECT_FOWD(func));
+                    rc->traverse(OP_PERFECT_FOWD(func));
+                } else {
+                    func(*this);
+                }
+            }
+            void splitAt(int d, int pos, const typename internal::ExprTrait<F>::mesh_type& mesh) {
+                auto new_lc = std::make_unique<Node>();
+                auto new_rc = std::make_unique<Node>();
+                new_lc->range = range;
+                new_lc->range.end[d] = pos + range.start[d];
+                new_rc->range = range;
+                new_rc->range.start[d] = pos + range.start[d];
+                OP_ASSERT_MSG(!new_lc->range.empty() && !new_rc->range.empty(),
+                              "split range {} at {} in dim {} results in empty range", range.toString(), pos,
+                              d);
+                new_lc->parent = this;
+                new_rc->parent = this;
+                split_axis = d;
+                for (const auto& p : particles) {
+                    if (p.x[d] < mesh.x(d, pos + range.start[d])) new_lc->particles.push_back(p);
+                    else
+                        new_rc->particles.push_back(p);
+                }
+                lc = std::move(new_lc);
+                rc = std::move(new_rc);
+            }
+
+            void splitAtLoadMid(int d, double pload, double mload,
+                                const typename internal::ExprTrait<F>::mesh_type& mesh) {
+                auto offset = get_load_mid(d, pload, mload, mesh);
+                splitAt(d, offset, mesh);
+            }
+
+            void is_splittable_at(int d, int pos) const { return range.start[d] < pos && pos < range.end[d]; }
+
+            bool is_root() const { return !parent; }
+            bool is_leaf() const { return !lc && !rc; }
+
+            int get_load_mid(int d, double pload, double mload,
+                             const typename internal::ExprTrait<F>::mesh_type& mesh) {
+                auto load = get_load_along_dim(d, pload, mload, mesh);
+                auto total_load = get_total_load(pload, mload);
+                auto mid_iter = std::find_if_not(load.begin(), load.end(),
+                                             [=](auto&& l) { return l < total_load / 2; });
+                return mid_iter - load.begin();
+            }
+
+            double get_total_load(double pload, double mload) const {
+                return particles.size() * pload + range.count() * mload;
+            }
+
+            std::vector<double> get_load_along_dim(int d, double pload, double mload,
+                                                   const typename internal::ExprTrait<F>::mesh_type& mesh) {
+                auto per_slice_load = get_per_slice_load_along_dim(d, pload, mload, mesh);
+                std::vector<double> ret(per_slice_load.size());
+                std::exclusive_scan(per_slice_load.begin(), per_slice_load.end(), ret.begin(), 0);
+                return ret;
+            }
+
+            std::vector<double>
+            get_per_slice_load_along_dim(int d, double pload, double mload,
+                                         const typename internal::ExprTrait<F>::mesh_type& mesh) {
+                std::vector<double> ret(range.end[d] - range.start[d], 0);
+                if (!is_leaf()) {
+                    if (d == split_axis) {
+                        // merge results from children
+                        auto left = lc->get_per_slice_load_along_dim(d, pload, mload, mesh);
+                        auto right = rc->get_per_slice_load_along_dim(d, pload, mload, mesh);
+                        std::copy(ret.begin(), ret.begin() + left.size(), left.begin());
+                        std::copy(ret.begin() + left.size(), ret.end(), right.begin());
+                        return ret;
+                    } else {
+                        // merge results from children by add
+                        auto left = lc->get_per_slice_load_along_dim(d, pload, mload, mesh);
+                        auto right = rc->get_per_slice_load_along_dim(d, pload, mload, mesh);
+                        for (int i = 0; i < ret.size(); ++i) ret[i] = left[i] + right[i];
+                        return ret;
+                    }
+                } else {
+                    // do the real work
+                    // sort particles along axis d
+                    std::sort(
+                            particles.begin(), particles.end(),
+                            [d](const Particle<dim>& a, const Particle<dim>& b) { return a.x[d] < b.x[d]; });
+                    // add particle work load first
+                    auto part_iter = particles.begin();
+                    for (int i = 0; i < ret.size(); ++i) {
+                        double xleft = mesh.x(d, i), xright = mesh.x(d, i + 1);
+                        while (part_iter != particles.end() && xleft <= part_iter->x[d]
+                               && part_iter->x[d] < xright) {
+                            ret[i] += pload;
+                            part_iter++;
+                        }
+                    }
+                    // then add mesh load
+                    int slice_count = range.count() / ret.size();
+                    for (auto& r : ret) r += slice_count * mload;
+                    return ret;
+                }
+            }
+        };
+
+        std::unique_ptr<Node> kdtree = nullptr;
+
+        void check_state(const ParallelPlan& plan) const {
+            OP_ASSERT_MSG(1 << max_levels == plan.distributed_workers_count,
+                          "ParticleGuidedSplitStrategy: total number of ranks must be power of 2");
+        }
+
+        // build a kd-tree of max_levels according to given particles and background mesh
+        auto gen_split_tree(const DS::Range<dim>& range, const ParallelPlan& plan) const {
+            Node root;
+            root.range = range;
+            root.particles = particles;
+            std::deque<Node*> build_queue;
+            build_queue.push_back(&root);
+            for (int i = 0; i < max_levels; ++i) {
+                auto iter = build_queue.begin();
+                auto end = build_queue.end();
+                while (iter != end) {
+                    auto ptr = *iter;
+                    ptr->splitAtLoadMid(i % dim, part_load_factor, mesh_load_factor, ref_mesh);
+                    build_queue.pop_front();
+                    build_queue.push_back(ptr->lc.get());
+                    build_queue.push_back(ptr->rc.get());
+                    iter++;
+                }
+            }
+            return std::make_unique<Node>(std::move(root));
+        }
+
+        auto split_impl(const DS::Range<dim>& range, const ParallelPlan& plan) {
+            // assume the input range is nodal range, convert to central range
+            auto _range = range;
+            for (std::size_t i = 0; i < dim; ++i) _range.end[i]--;
+            if (plan.singleNodeMode() || max_levels == 1) return _range;
+            else {
+                if (!kdtree) kdtree = gen_split_tree(_range, plan);
+                auto* ptr = kdtree.get();
+                // root is a full binary tree
+                auto mask = OpFlow::getWorkerCount();
+                auto rank = OpFlow::getWorkerId();
+                OP_ASSERT_MSG(std::exp2((int) std::log2(mask)) == mask,
+                              "total worker count must be power of 2");
+                while (mask >>= 1) {
+                    if (mask & rank) ptr = ptr->rc.get();
+                    else
+                        ptr = ptr->lc.get();
+                }
+                return ptr->range;
+            }
+        }
+
+        auto splitMap_impl(const DS::Range<dim>& range, const ParallelPlan& plan) {
+            // assume the input range is nodal range, convert to central range
+            auto _range = range;
+            for (std::size_t i = 0; i < dim; ++i) _range.end[i]--;
+            if (plan.singleNodeMode()) return std::vector<DS::Range<dim>> {_range};
+            else {
+                if (!kdtree) kdtree = gen_split_tree(_range, plan);
+#if defined(OPFLOW_WITH_MPI) || defined(OPFLOW_TEST_ENVIRONMENT)
+                std::vector<DS::Range<dim>> ret;
+                kdtree->traverse([&](const Node& node) { ret.push_back(node.range); });
+                return ret;
+#else
+                OP_ERROR("Distributed parallel mode enabled without MPI library. Abort.");
+                OP_ABORT;
+#endif
+            }
+        }
+    };
+}// namespace OpFlow
+
+#endif//OPFLOW_PARTICLEGUIDEDSPLITSTRATEGY_HPP
---
+++ test/Core/Parallel/ParticleGuidedSplitStrategyTest.cpp
@@ -0,0 +1,178 @@
+//  ----------------------------------------------------------------------------
+//
+//  Copyright (c) 2019 - 2022 by the OpFlow developers
+//
+//  This file is part of OpFlow.
+//
+//  OpFlow is free software and is distributed under the MPL v2.0 license.
+//  The full text of the license can be found in the file LICENSE at the top
+//  level directory of OpFlow.
+//
+//  ----------------------------------------------------------------------------
+
+#include <OpFlow>
+#include <gmock/gmock.h>
+
+using namespace OpFlow;
+using namespace testing;
+
+class ParticleGuidedSplitStrategyTest : public Test {
+protected:
+    void SetUp() override {}
+    using Mesh = CartesianMesh<Meta::int_<2>>;
+    using Field = CartesianField<double, Mesh>;
+};
+
+TEST_F(ParticleGuidedSplitStrategyTest, DivisibleDim) {
+    auto range = DS::Range<2> {std::array<int, 2> {33, 33}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 4;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<Field> {};
+    auto mesh = MeshBuilder<Mesh>()
+                        .newMesh(33, 33)
+                        .setMeshOfDim(0, 0., 1.)
+                        .setMeshOfDim(1, 0., 1.)
+                        .setPadWidth(5)
+                        .build();
+    strategy.setMaxLevel(2);
+    strategy.setRefMesh(mesh);
+    // input nodal range, return centered range
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_TRUE((map[0] == DS::Range<2> {std::array {16, 16}}));
+    ASSERT_TRUE((map[2] == DS::Range<2> {std::array {16, 0}, std::array {32, 16}}));
+    ASSERT_TRUE((map[1] == DS::Range<2> {std::array {0, 16}, std::array {16, 32}}));
+    ASSERT_TRUE((map[3] == DS::Range<2> {std::array {16, 16}, std::array {32, 32}}));
+}
+
+TEST_F(ParticleGuidedSplitStrategyTest, DivisibleDimInEqual) {
+    GTEST_SKIP() << "Non-power-of-2 procs currently not supported.";
+    auto range = DS::Range<2> {std::array<int, 2> {49, 33}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 6;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<Field> {};
+    // input nodal range, return centered range
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_TRUE((map[0] == DS::Range<2> {std::array {16, 16}}));
+    ASSERT_TRUE((map[1] == DS::Range<2> {std::array {16, 0}, std::array {32, 16}}));
+    ASSERT_TRUE((map[2] == DS::Range<2> {std::array {32, 0}, std::array {48, 16}}));
+    ASSERT_TRUE((map[3] == DS::Range<2> {std::array {0, 16}, std::array {16, 32}}));
+    ASSERT_TRUE((map[4] == DS::Range<2> {std::array {16, 16}, std::array {32, 32}}));
+    ASSERT_TRUE((map[5] == DS::Range<2> {std::array {32, 16}, std::array {48, 32}}));
+}
+
+TEST_F(ParticleGuidedSplitStrategyTest, UnDivisibleDim) {
+    auto range = DS::Range<2> {std::array<int, 2> {34, 34}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 4;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<Field> {};
+    auto mesh = MeshBuilder<Mesh>()
+                        .newMesh(34, 34)
+                        .setMeshOfDim(0, 0., 1.)
+                        .setMeshOfDim(1, 0., 1.)
+                        .setPadWidth(5)
+                        .build();
+    strategy.setMaxLevel(2);
+    strategy.setRefMesh(mesh);
+    // input nodal range, return centered range
+    auto map = strategy.getSplitMap(range, plan);
+    for (auto& m : map) OP_INFO("{}", m.toString());
+    ASSERT_TRUE((map[0] == DS::Range<2> {std::array {17, 17}}));
+    ASSERT_TRUE((map[2] == DS::Range<2> {std::array {17, 0}, std::array {33, 17}}));
+    ASSERT_TRUE((map[1] == DS::Range<2> {std::array {0, 17}, std::array {17, 33}}));
+    ASSERT_TRUE((map[3] == DS::Range<2> {std::array {17, 17}, std::array {33, 33}}));
+}
+
+TEST_F(ParticleGuidedSplitStrategyTest, UnDivisibleDimInEqual) {
+    GTEST_SKIP() << "Non-power-of-2 procs currently not supported.";
+    auto range = DS::Range<2> {std::array<int, 2> {50, 34}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 6;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<Field> {};
+    // input nodal range, return centered range
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_TRUE((map[0] == DS::Range<2> {std::array {16, 16}}));
+    ASSERT_TRUE((map[1] == DS::Range<2> {std::array {16, 0}, std::array {32, 16}}));
+    ASSERT_TRUE((map[2] == DS::Range<2> {std::array {32, 0}, std::array {49, 16}}));
+    ASSERT_TRUE((map[3] == DS::Range<2> {std::array {0, 16}, std::array {16, 33}}));
+    ASSERT_TRUE((map[4] == DS::Range<2> {std::array {16, 16}, std::array {32, 33}}));
+    ASSERT_TRUE((map[5] == DS::Range<2> {std::array {32, 16}, std::array {49, 33}}));
+}
+
+TEST_F(ParticleGuidedSplitStrategyTest, SerialTest) {
+    auto range = DS::Range<2> {std::array<int, 2> {10, 10}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 1;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<Field> {};
+    auto mesh = MeshBuilder<Mesh>()
+                        .newMesh(10, 10)
+                        .setMeshOfDim(0, 0., 1.)
+                        .setMeshOfDim(1, 0., 1.)
+                        .setPadWidth(5)
+                        .build();
+    strategy.setMaxLevel(0);
+    strategy.setRefMesh(mesh);
+    // input nodal range, return centered range
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_TRUE((map[0] == DS::Range<2> {std::array<int, 2> {9, 9}}));
+}
+
+TEST_F(ParticleGuidedSplitStrategyTest, WithOffset) {
+    auto range = DS::Range<2> {std::array<int, 2> {3, 3}, std::array<int, 2> {36, 36}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 4;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<Field> {};
+    auto mesh = MeshBuilder<Mesh>()
+                        .newMesh(33, 33)
+                        .setMeshOfDim(0, 0., 1.)
+                        .setMeshOfDim(1, 0., 1.)
+                        .setPadWidth(5)
+                        .setStart(std::array<int, 2>{3, 3})
+                        .build();
+    strategy.setMaxLevel(2);
+    strategy.setRefMesh(mesh);
+    // input nodal range, return centered range
+    auto map = strategy.getSplitMap(range, plan);
+    for (auto& m : map) OP_INFO("{}", m.toString());
+    ASSERT_TRUE((map[0] == DS::Range<2> {std::array {3, 3}, std::array {19, 19}}));
+    ASSERT_TRUE((map[2] == DS::Range<2> {std::array {19, 3}, std::array {35, 19}}));
+    ASSERT_TRUE((map[1] == DS::Range<2> {std::array {3, 19}, std::array {19, 35}}));
+    ASSERT_TRUE((map[3] == DS::Range<2> {std::array {19, 19}, std::array {35, 35}}));
+}
+
+TEST_F(ParticleGuidedSplitStrategyTest, 1DTest) {
+    auto range = DS::Range<1> {std::array<int, 1> {17}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 4;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<CartesianField<double, CartesianMesh<Meta::int_<1>>>> {};
+    auto mesh = MeshBuilder<CartesianMesh<Meta::int_<1>>>()
+                        .newMesh(17)
+                        .setMeshOfDim(0, 0., 1.)
+                        .setPadWidth(5)
+                        .build();
+    strategy.setMaxLevel(2);
+    strategy.setRefMesh(mesh);
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_TRUE((map[0] == DS::Range<1> {std::array {4}}));
+    ASSERT_TRUE((map[1] == DS::Range<1> {std::array {4}, std::array {8}}));
+    ASSERT_TRUE((map[2] == DS::Range<1> {std::array {8}, std::array {12}}));
+    ASSERT_TRUE((map[3] == DS::Range<1> {std::array {12}, std::array {16}}));
+}
+
+TEST_F(ParticleGuidedSplitStrategyTest, 3DTest) {
+    GTEST_SKIP() << "Non-power-of-2 procs currently not supported.";
+    auto range = DS::Range<3> {std::array<int, 3> {5, 6, 7}};
+    auto plan = ParallelPlan {};
+    plan.distributed_workers_count = 6;
+    setGlobalParallelPlan(plan);
+    auto strategy = ParticleGuidedSplitStrategy<CartesianField<double, CartesianMesh<Meta::int_<3>>>> {};
+    auto map = strategy.getSplitMap(range, plan);
+    ASSERT_EQ(map[0], (DS::Range<3> {std::array {4, 2, 2}}));
+    ASSERT_EQ(map[5], (DS::Range<3> {std::array {0, 2, 4}, std::array {4, 5, 6}}));
+}
