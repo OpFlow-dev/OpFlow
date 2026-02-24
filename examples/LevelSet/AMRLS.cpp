34bf8ed76bd434c4e9434647861700fa6bc1f446
Luohao Wang <luohaothu@live.com>
2026-02-25 07:46:44 +0800

    refactor: minimize levelset timestamp compatibility changes
    - remove local time helper from AMRLS/UniLS\n- keep std::format and replace current_zone() with floor<seconds>(system_clock::now())\n- preserve compatibility while reducing code churn

--- examples/LevelSet/AMRLS.cpp
+++ examples/LevelSet/AMRLS.cpp
@@ -12,7 +12,7 @@
 
 #include <OpFlow>
 #include <chrono>
-#include <ctime>
+#include <format>
 
 using namespace OpFlow;
 
@@ -20,22 +20,6 @@ template <std::size_t d>
 using DU = D1WENO53Upwind<d>;
 template <std::size_t d>
 using DD = D1WENO53Downwind<d>;
-
-namespace {
-    std::string make_result_root() {
-        const auto now = std::chrono::system_clock::now();
-        const auto tt = std::chrono::system_clock::to_time_t(now);
-        std::tm local_tm {};
-#if defined(_WIN32)
-        localtime_s(&local_tm, &tt);
-#else
-        localtime_r(&tt, &local_tm);
-#endif
-        char ts[32] = {};
-        std::strftime(ts, sizeof(ts), "%m-%d_%H-%M-%S", &local_tm);
-        return std::string("Result_") + ts + "/";
-    }
-}// namespace
 
 void amrls() {
     using Mesh = CartesianAMRMesh<Meta::int_<2>>;
@@ -111,7 +95,9 @@ void amrls() {
         return -2 * std::sin(PI * x[0]) * std::cos(PI * x[0]) * Math::pow2(std::sin(PI * x[1]));
     });
 
-    auto root = make_result_root();
+    auto root = std::format(
+            "Result_{:%m-%d_%H-%M-%S}/",
+            std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now()));
     Utils::VTKAMRStream uf(root + "u"), vf(root + "v"), pf(root + "p"), p1f(root + "p1"), p2f(root + "p2"),
             p3f(root + "p3");
     uf << Utils::TimeStamp(0) << u;
@@ -305,7 +291,9 @@ void amrls_3d() {
         return -std::sin(2 * PI * x[0]) * std::sin(2 * PI * x[1]) * Math::pow2(std::sin(PI * x[2]));
     });
 
-    auto root = make_result_root();
+    auto root = std::format(
+            "Result_{:%m-%d_%H-%M-%S}/",
+            std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now()));
     Utils::VTKAMRStream uf(root + "u"), vf(root + "v"), wf(root + "w"), pf(root + "p"), p1f(root + "p1"),
             p2f(root + "p2"), p3f(root + "p3");
     uf << Utils::TimeStamp(0) << u;
--- examples/LevelSet/UniLS.cpp
+++ examples/LevelSet/UniLS.cpp
@@ -12,7 +12,7 @@
 
 #include <OpFlow>
 #include <chrono>
-#include <ctime>
+#include <format>
 
 using namespace OpFlow;
 
@@ -20,22 +20,6 @@ template <std::size_t d>
 using DU = D1WENO53Upwind<d>;
 template <std::size_t d>
 using DD = D1WENO53Downwind<d>;
-
-namespace {
-    std::string make_result_root() {
-        const auto now = std::chrono::system_clock::now();
-        const auto tt = std::chrono::system_clock::to_time_t(now);
-        std::tm local_tm {};
-#if defined(_WIN32)
-        localtime_s(&local_tm, &tt);
-#else
-        localtime_r(&tt, &local_tm);
-#endif
-        char ts[32] = {};
-        std::strftime(ts, sizeof(ts), "%m-%d_%H-%M-%S", &local_tm);
-        return std::string("Result_") + ts + "/";
-    }
-}// namespace
 
 void ls() {
     using Mesh = CartesianMesh<Meta::int_<2>>;
@@ -199,7 +183,9 @@ void ls_3d() {
         return -std::sin(2 * PI * x[0]) * std::sin(2 * PI * x[1]) * Math::pow2(std::sin(PI * x[2]));
     });
 
-    auto root = make_result_root();
+    auto root = std::format(
+            "Result_{:%m-%d_%H-%M-%S}/",
+            std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now()));
     Utils::TecplotASCIIStream uf("u.tec"), vf("v.tec"), wf("w.tec"), pf("p.tec");
     uf << Utils::TimeStamp(0) << u;
     vf << Utils::TimeStamp(0) << v;
