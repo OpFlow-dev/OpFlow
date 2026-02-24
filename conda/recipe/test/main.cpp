263e30cac1382799018346ac5f4ec776f22baef6
Luohao Wang <luohaothu@live.com>
2026-02-25 00:23:28 +0800

    fix: use exported version string in conda smoke test
    Replace removed OPFLOW_VERSION macro check with OPFLOW_VERSION_STRING assertion.

--- conda/recipe/test/main.cpp
+++ conda/recipe/test/main.cpp
@@ -1,6 +1,6 @@
 #include <Version.hpp>
 
 int main() {
-    static_assert(OPFLOW_VERSION > 0);
-    return OPFLOW_VERSION == 0;
+    static_assert(OpFlow::internal::OPFLOW_VERSION_STRING.size() > 0);
+    return OpFlow::internal::OPFLOW_VERSION_STRING.empty();
 }
